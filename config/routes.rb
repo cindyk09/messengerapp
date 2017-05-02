86f7152763b6e50f22f3eb40328ae7111d0f9349
cindyk09 <cindyk09@gmail.com>
2017-05-01 23:50:03 -0400

    added devise gem and working with before_filter authenticate_user!

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :chats
   resources :messages
   resources :users
+  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

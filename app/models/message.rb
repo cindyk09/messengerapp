a820dfc027911950bc06a7c4ea8ddeafb804f9a6
cindyk09 <cindyk09@gmail.com>
2017-05-02 16:12:19 -0400

    basic functionatlity is working. You can message others

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :chat
   belongs_to :user
+
+  validates_presence_of :body, :chat_id, :user_id
+
+   def message_time
+    created_at.strftime ("%m/%d/%y at %l:%M %p")
+   end
 end

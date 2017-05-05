105e03abdd0f9f6f41232b150e957c1facce114b
cindyk09 <cindyk09@gmail.com>
2017-05-05 12:17:56 -0400

    websockets and real time chat is workinggit add .!

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -4,26 +4,18 @@ module ApplicationCable
 
     def connect
       self.current_user = find_verified_user
-      logger.add_tags 'ActionCable', current_user.first_name
+      logger.add_tags 'ActionCable', current_user.email
     end
 
     protected
     def find_verified_user
-      verified_user = User.find_by(id: cookies.signed['user.id'])
-      if verified_user && cookies.signed['user.expires_at'] > Time.now
+      # Warden uses strategies to try to authenticate sessions. couldn't set current_user through cookies
+      if verified_user = env['warden'].user
         verified_user
       else
         reject_unauthorized_connection
       end
     end
 
-
-      # def find_verified_user
-      #   if current_user = User.find_by(id: cookies.signed[:user_id])
-      #     current_user
-      #   else
-      #     reject_unauthorized_connection
-      #   end
-      # end
   end
 end
--- config/initializers/warden_hooks.rb
+++
@@ -1,5 +0,0 @@
-Warden::Manager.before_logout do |user, auth, opts|
-  scope = opts[:scope]
-  auth.cookies.signed["#{scope}.id"] = nil
-  auth.cookies.signed["#{scope}.expires_at"] = nil
-end
